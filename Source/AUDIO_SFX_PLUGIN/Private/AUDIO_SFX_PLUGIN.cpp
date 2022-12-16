7849edba87dcf76a78304fd80be5f4a9279f5f2b
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-16 04:02:03 -0500

    Update AUDIO_SFX_PLUGIN.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/AUDIO_SFX_PLUGIN.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/AUDIO_SFX_PLUGIN.cpp
@@ -15,95 +15,95 @@ static const FName AUDIO_SFX_PLUGINTabName("AUDIO_SFX_PLUGIN");
 
 void FAUDIO_SFX_PLUGINModule::StartupModule()
 {
-	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
-	
-	FAUDIO_SFX_PLUGINStyle::Initialize();
-	FAUDIO_SFX_PLUGINStyle::ReloadTextures();
+    // This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+    
+    FAUDIO_SFX_PLUGINStyle::Initialize();
+    FAUDIO_SFX_PLUGINStyle::ReloadTextures();
 
-	FAUDIO_SFX_PLUGINCommands::Register();
-	
-	PluginCommands = MakeShareable(new FUICommandList);
+    FAUDIO_SFX_PLUGINCommands::Register();
+    
+    PluginCommands = MakeShareable(new FUICommandList);
 
-	PluginCommands->MapAction(
-		FAUDIO_SFX_PLUGINCommands::Get().OpenPluginWindow,
-		FExecuteAction::CreateRaw(this, &FAUDIO_SFX_PLUGINModule::PluginButtonClicked),
-		FCanExecuteAction());
+    PluginCommands->MapAction(
+        FAUDIO_SFX_PLUGINCommands::Get().OpenPluginWindow,
+        FExecuteAction::CreateRaw(this, &FAUDIO_SFX_PLUGINModule::PluginButtonClicked),
+        FCanExecuteAction());
 
-	UToolMenus::RegisterStartupCallback(FSimpleMulticastDelegate::FDelegate::CreateRaw(this, &FAUDIO_SFX_PLUGINModule::RegisterMenus));
-	
-	FGlobalTabmanager::Get()->RegisterNomadTabSpawner(AUDIO_SFX_PLUGINTabName, FOnSpawnTab::CreateRaw(this, &FAUDIO_SFX_PLUGINModule::OnSpawnPluginTab))
-		.SetDisplayName(LOCTEXT("FAUDIO_SFX_PLUGINTabTitle", "AUDIO_SFX_PLUGIN"))
-		.SetMenuType(ETabSpawnerMenuType::Hidden);
+    UToolMenus::RegisterStartupCallback(FSimpleMulticastDelegate::FDelegate::CreateRaw(this, &FAUDIO_SFX_PLUGINModule::RegisterMenus));
+    
+    FGlobalTabmanager::Get()->RegisterNomadTabSpawner(AUDIO_SFX_PLUGINTabName, FOnSpawnTab::CreateRaw(this, &FAUDIO_SFX_PLUGINModule::OnSpawnPluginTab))
+        .SetDisplayName(LOCTEXT("FAUDIO_SFX_PLUGINTabTitle", "AUDIO_SFX_PLUGIN"))
+        .SetMenuType(ETabSpawnerMenuType::Hidden);
 }
 
 void FAUDIO_SFX_PLUGINModule::ShutdownModule()
 {
-	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-	// we call this function before unloading the module.
+    // This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+    // we call this function before unloading the module.
 
-	UToolMenus::UnRegisterStartupCallback(this);
+    UToolMenus::UnRegisterStartupCallback(this);
 
-	UToolMenus::UnregisterOwner(this);
+    UToolMenus::UnregisterOwner(this);
 
-	FAUDIO_SFX_PLUGINStyle::Shutdown();
+    FAUDIO_SFX_PLUGINStyle::Shutdown();
 
-	FAUDIO_SFX_PLUGINCommands::Unregister();
+    FAUDIO_SFX_PLUGINCommands::Unregister();
 
-	FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(AUDIO_SFX_PLUGINTabName);
+    FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(AUDIO_SFX_PLUGINTabName);
 }
 
 TSharedRef<SDockTab> FAUDIO_SFX_PLUGINModule::OnSpawnPluginTab(const FSpawnTabArgs& SpawnTabArgs)
 {
-	FText WidgetText = FText::Format(
-		LOCTEXT("WindowWidgetText", "Add code to {0} in {1} to override this window's contents"),
-		FText::FromString(TEXT("FAUDIO_SFX_PLUGINModule::OnSpawnPluginTab")),
-		FText::FromString(TEXT("AUDIO_SFX_PLUGIN.cpp"))
-		);
+    FText WidgetText = FText::Format(
+        LOCTEXT("WindowWidgetText", "Add code to {0} in {1} to override this window's contents"),
+        FText::FromString(TEXT("FAUDIO_SFX_PLUGINModule::OnSpawnPluginTab")),
+        FText::FromString(TEXT("AUDIO_SFX_PLUGIN.cpp"))
+        );
 
-	return SNew(SDockTab)
-		.TabRole(ETabRole::NomadTab)
-		[
-			// Put your tab content here!
-			SNew(SBox)
-			.HAlign(HAlign_Center)
-			.VAlign(VAlign_Center)
-			[
-				SNew(STextBlock)
-				.Text(WidgetText)
-			]
-		];
+    return SNew(SDockTab)
+        .TabRole(ETabRole::MajorTab)
+        [
+            // Put your tab content here!
+            SNew(SBox)
+            .HAlign(HAlign_Center)
+            .VAlign(VAlign_Center)
+            [
+                SNew(STextBlock)
+                .Text(WidgetText)
+            ]
+        ];
 }
 
 void FAUDIO_SFX_PLUGINModule::PluginButtonClicked()
 {
-	FGlobalTabmanager::Get()->TryInvokeTab(AUDIO_SFX_PLUGINTabName);
+    FGlobalTabmanager::Get()->TryInvokeTab(AUDIO_SFX_PLUGINTabName);
 }
 
 void FAUDIO_SFX_PLUGINModule::RegisterMenus()
 {
-	// Owner will be used for cleanup in call to UToolMenus::UnregisterOwner
-	FToolMenuOwnerScoped OwnerScoped(this);
+    // Owner will be used for cleanup in call to UToolMenus::UnregisterOwner
+    FToolMenuOwnerScoped OwnerScoped(this);
 
-	{
-		UToolMenu* Menu = UToolMenus::Get()->ExtendMenu("LevelEditor.MainMenu.Window");
-		{
-			FToolMenuSection& Section = Menu->FindOrAddSection("WindowLayout");
-			Section.AddMenuEntryWithCommandList(FAUDIO_SFX_PLUGINCommands::Get().OpenPluginWindow, PluginCommands);
-		}
-	}
+    {
+        UToolMenu* Menu = UToolMenus::Get()->ExtendMenu("LevelEditor.MainMenu.Window");
+        {
+            FToolMenuSection& Section = Menu->FindOrAddSection("WindowLayout");
+            Section.AddMenuEntryWithCommandList(FAUDIO_SFX_PLUGINCommands::Get().OpenPluginWindow, PluginCommands);
+        }
+    }
 
-	{
-		UToolMenu* ToolbarMenu = UToolMenus::Get()->ExtendMenu("LevelEditor.LevelEditorToolBar");
-		{
-			FToolMenuSection& Section = ToolbarMenu->FindOrAddSection("Settings");
-			{
-				FToolMenuEntry& Entry = Section.AddEntry(FToolMenuEntry::InitToolBarButton(FAUDIO_SFX_PLUGINCommands::Get().OpenPluginWindow));
-				Entry.SetCommandList(PluginCommands);
-			}
-		}
-	}
+    {
+        UToolMenu* ToolbarMenu = UToolMenus::Get()->ExtendMenu("LevelEditor.LevelEditorToolBar");
+        {
+            FToolMenuSection& Section = ToolbarMenu->FindOrAddSection("Settings");
+            {
+                FToolMenuEntry& Entry = Section.AddEntry(FToolMenuEntry::InitToolBarButton(FAUDIO_SFX_PLUGINCommands::Get().OpenPluginWindow));
+                Entry.SetCommandList(PluginCommands);
+            }
+        }
+    }
 }
 
 #undef LOCTEXT_NAMESPACE
-	
+    
 IMPLEMENT_MODULE(FAUDIO_SFX_PLUGINModule, AUDIO_SFX_PLUGIN)
