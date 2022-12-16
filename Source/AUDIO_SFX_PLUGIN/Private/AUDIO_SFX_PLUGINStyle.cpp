8d97fd791602ebda75eda7945a02314f3f556321
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-16 04:01:58 -0500

    Update AUDIO_SFX_PLUGINStyle.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/AUDIO_SFX_PLUGINStyle.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/AUDIO_SFX_PLUGINStyle.cpp
@@ -13,24 +13,24 @@ TSharedPtr<FSlateStyleSet> FAUDIO_SFX_PLUGINStyle::StyleInstance = nullptr;
 
 void FAUDIO_SFX_PLUGINStyle::Initialize()
 {
-	if (!StyleInstance.IsValid())
-	{
-		StyleInstance = Create();
-		FSlateStyleRegistry::RegisterSlateStyle(*StyleInstance);
-	}
+    if (!StyleInstance.IsValid())
+    {
+        StyleInstance = Create();
+        FSlateStyleRegistry::RegisterSlateStyle(*StyleInstance);
+    }
 }
 
 void FAUDIO_SFX_PLUGINStyle::Shutdown()
 {
-	FSlateStyleRegistry::UnRegisterSlateStyle(*StyleInstance);
-	ensure(StyleInstance.IsUnique());
-	StyleInstance.Reset();
+    FSlateStyleRegistry::UnRegisterSlateStyle(*StyleInstance);
+    ensure(StyleInstance.IsUnique());
+    StyleInstance.Reset();
 }
 
 FName FAUDIO_SFX_PLUGINStyle::GetStyleSetName()
 {
-	static FName StyleSetName(TEXT("AUDIO_SFX_PLUGINStyle"));
-	return StyleSetName;
+    static FName StyleSetName(TEXT("AUDIO_SFX_PLUGINStyle"));
+    return StyleSetName;
 }
 
 const FVector2D Icon16x16(16.0f, 16.0f);
@@ -38,23 +38,23 @@ const FVector2D Icon20x20(20.0f, 20.0f);
 
 TSharedRef< FSlateStyleSet > FAUDIO_SFX_PLUGINStyle::Create()
 {
-	TSharedRef< FSlateStyleSet > Style = MakeShareable(new FSlateStyleSet("AUDIO_SFX_PLUGINStyle"));
-	Style->SetContentRoot(IPluginManager::Get().FindPlugin("AUDIO_SFX_PLUGIN")->GetBaseDir() / TEXT("Resources"));
+    TSharedRef< FSlateStyleSet > Style = MakeShareable(new FSlateStyleSet("AUDIO_SFX_PLUGINStyle"));
+    Style->SetContentRoot(IPluginManager::Get().FindPlugin("AUDIO_SFX_PLUGIN")->GetBaseDir() / TEXT("Resources"));
 
-	Style->Set("AUDIO_SFX_PLUGIN.OpenPluginWindow", new IMAGE_BRUSH_SVG(TEXT("PlaceholderButtonIcon"), Icon20x20));
+    Style->Set("AUDIO_SFX_PLUGIN.OpenPluginWindow", new IMAGE_BRUSH_SVG(TEXT("PlaceholderButtonIcon"), Icon20x20));
 
-	return Style;
+    return Style;
 }
 
 void FAUDIO_SFX_PLUGINStyle::ReloadTextures()
 {
-	if (FSlateApplication::IsInitialized())
-	{
-		FSlateApplication::Get().GetRenderer()->ReloadTextureResources();
-	}
+    if (FSlateApplication::IsInitialized())
+    {
+        FSlateApplication::Get().GetRenderer()->ReloadTextureResources();
+    }
 }
 
 const ISlateStyle& FAUDIO_SFX_PLUGINStyle::Get()
 {
-	return *StyleInstance;
+    return *StyleInstance;
 }
