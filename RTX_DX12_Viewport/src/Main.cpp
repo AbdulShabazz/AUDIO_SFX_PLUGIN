9dbc9b26120a0016811c2dcf96605a5f74f4490e
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-21 01:57:28 -0500

    Update Main.cpp

--- RTX_DX12_Viewport/src/Main.cpp
+++ RTX_DX12_Viewport/src/Main.cpp
@@ -1,4 +1,41 @@
+#include <chrono>
+#include <functional>
+#include <thread>
+
 #include "DXRApplication.h"
+
+struct ParamSTRUCT
+{
+    MSG msg = { 0 };
+    DXRApplication app;
+};
+
+void set_interval(
+    std::function<void(ParamSTRUCT&)> callback,
+    ParamSTRUCT& ParamStruct,
+    std::chrono::milliseconds intervalInt64
+)
+{
+    // Set Timer
+    while (WM_QUIT != ParamStruct.msg.message)
+    {
+        callback(ParamStruct);
+        std::this_thread::sleep_for(intervalInt64);
+    }
+    ParamStruct.app.Cleanup();
+}
+
+void RenderCallback(ParamSTRUCT& ParamStruct)
+{
+    if (PeekMessage(&ParamStruct.msg, NULL, 0, 0, PM_REMOVE))
+    {
+        TranslateMessage(&ParamStruct.msg);
+        DispatchMessage(&ParamStruct.msg);
+    }
+
+    ParamStruct.app.Update();
+    ParamStruct.app.Render();
+}
 
 /**
  * Program entry point.
@@ -21,23 +58,16 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         if (hr != EXIT_SUCCESS) return hr;
 
         // Initialize
-        DXRApplication app;
-        app.Init(config);
+        ParamSTRUCT ParamStruct;
+        ParamStruct.msg = msg;
+        ParamStruct.app.Init(config);
 
         // Main loop
-        while (WM_QUIT != msg.message)
-        {
-            if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
-            {
-                TranslateMessage(&msg);
-                DispatchMessage(&msg);
-            }
-
-            app.Update();
-            app.Render();
-        }
-
-        app.Cleanup();
+        set_interval(
+            RenderCallback,
+            ParamStruct,
+            std::chrono::milliseconds(1000 / 2000) // eg. 1000/60 = 60fps
+        );
     }
 
 #if defined _CRTDBG_MAP_ALLOC
