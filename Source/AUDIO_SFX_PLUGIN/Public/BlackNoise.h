7ce99337bb4728f8c40afb3738c55bf1bdc3892e
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-25 17:40:51 -0500

    Update BlackNoise.h

--- Source/AUDIO_SFX_PLUGIN/Public/BlackNoise.h
+++ Source/AUDIO_SFX_PLUGIN/Public/BlackNoise.h
@@ -1,7 +1,12 @@
 #pragma once
 
+struct FILEINFO_Obj;
+
 class BlackNoise
 {
 public:
     BlackNoise();
+    void GenerateBlackNoise(FILEINFO_Obj& FileInfoObj);
+protected:
+    void ApplyBlackFilter(FILEINFO_Obj& FileInfoObj);
 };
