b773f705453e9d50cadca539199804aa4edcc85f
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-25 17:39:59 -0500

    Update BlackNoise.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/BlackNoise.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/BlackNoise.cpp
@@ -1,5 +1,14 @@
 #include "BlackNoise.h"
+#include "Tones.Default.Settings.h"
 
 BlackNoise::BlackNoise()
+{
+}
+
+void BlackNoise::GenerateBlackNoise(FILEINFO_Obj& FileInfoObj)
+{
+}
+
+void BlackNoise::ApplyBlackFilter(FILEINFO_Obj& FileInfoObj)
 {
 }
