67213b112fab7713b9ce0ae0ffa954ae660b4deb
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-25 17:40:05 -0500

    Update BlueNoise.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/BlueNoise.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/BlueNoise.cpp
@@ -1,5 +1,14 @@
 #include "BlueNoise.h"
+#include "Tones.Default.Settings.h"
 
 BlueNoise::BlueNoise()
+{
+}
+
+void BlueNoise::GenerateBlueNoise(FILEINFO_Obj& FileInfoObj)
+{
+}
+
+void BlueNoise::ApplyBlueFilter(FILEINFO_Obj& FileInfoObj)
 {
 }
