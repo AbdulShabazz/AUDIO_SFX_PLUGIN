c47f901f4d7153151cbb7cb7b9d807897b2a2800
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-25 17:41:13 -0500

    Update Tones.Default.Settings.h

--- Source/AUDIO_SFX_PLUGIN/Public/Tones.Default.Settings.h
+++ Source/AUDIO_SFX_PLUGIN/Public/Tones.Default.Settings.h
@@ -6,6 +6,13 @@
 struct SETTINGS_OBJ
 {
     double SampleRateInt64 = 44100;
-    int TableSizeInt32 = 256;
-    int AmplitudeInt32 = 0.8;
+    int TableSizeInt16 = 256;
+    int AmplitudeInt16 = 0.8;
 } TonesSettingsObj;
+
+struct FILEINFO_Obj
+{
+    float* NoiseBufferInt32;
+    int LengthInt16;
+
+};
