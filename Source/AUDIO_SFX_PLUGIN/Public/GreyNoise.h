eed643e5559ef5739b922f127453710a2a2085d8
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-25 17:40:58 -0500

    Update GreyNoise.h

--- Source/AUDIO_SFX_PLUGIN/Public/GreyNoise.h
+++ Source/AUDIO_SFX_PLUGIN/Public/GreyNoise.h
@@ -1,7 +1,12 @@
 #pragma once
 
+struct FILEINFO_Obj;
+
 class GreyNoise
 {
 public:
     GreyNoise();
+    void GenerateGreyNoise(FILEINFO_Obj& FileInfoObj);
+protected:
+    void ApplyGreyFilter(FILEINFO_Obj& FileInfoObj);
 };
