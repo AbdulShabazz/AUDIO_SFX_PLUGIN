11c3b84250c03d9bc83dbdc6d279235cc870a6d5
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-25 17:40:54 -0500

    Update BlueNoise.h

--- Source/AUDIO_SFX_PLUGIN/Public/BlueNoise.h
+++ Source/AUDIO_SFX_PLUGIN/Public/BlueNoise.h
@@ -1,7 +1,12 @@
 #pragma once
 
+struct FILEINFO_Obj;
+
 class BlueNoise
 {
 public:
     BlueNoise();
+    void GenerateBlueNoise(FILEINFO_Obj& FileInfoObj);
+protected:
+    void ApplyBlueFilter(FILEINFO_Obj& FileInfoObj);
 };
