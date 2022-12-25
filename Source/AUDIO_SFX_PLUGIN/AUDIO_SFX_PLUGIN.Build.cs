7d32091d7fcafc47277605dc4e812624d60ae9cc
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-25 17:39:46 -0500

    Update AUDIO_SFX_PLUGIN.Build.cs

--- Source/AUDIO_SFX_PLUGIN/AUDIO_SFX_PLUGIN.Build.cs
+++ Source/AUDIO_SFX_PLUGIN/AUDIO_SFX_PLUGIN.Build.cs
@@ -43,6 +43,7 @@ public class AUDIO_SFX_PLUGIN : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+				"UMG"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
