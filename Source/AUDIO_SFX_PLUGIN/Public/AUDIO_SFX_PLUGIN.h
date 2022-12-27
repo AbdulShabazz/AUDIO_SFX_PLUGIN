0ce4c7929eb44b604d1cb65adbaeb0c1f8ed5a1f
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-27 18:04:12 -0500

    Update AUDIO_SFX_PLUGIN.h

--- Source/AUDIO_SFX_PLUGIN/Public/AUDIO_SFX_PLUGIN.h
+++ Source/AUDIO_SFX_PLUGIN/Public/AUDIO_SFX_PLUGIN.h
@@ -19,9 +19,18 @@ public:
     /** IModuleInterface implementation */
     virtual void StartupModule() override;
     virtual void ShutdownModule() override;
+    void CreatCameraViewportPlayerControllerEtc();
     
     /** This function will be bound to Command (by default it will bring up plugin window) */
     void PluginButtonClicked();
+
+private:
+    TObjectPtr<UWorld> World;
+    TObjectPtr<ACameraActor> Camera;
+    TObjectPtr<APlayerController> PlayerController;
+    //TObjectPtr<UGameViewportClient> ViewportClient;
+    TObjectPtr<FViewportClient> ViewportClient;
+    TObjectPtr<FViewport> Viewport;
     
 private:
 
@@ -31,57 +40,4 @@ private:
 
 private:
     TSharedPtr<class FUICommandList> PluginCommands;
-};
-
-class FMYViewport : public FViewport
-{
-public:
-    FMYViewport(TObjectPtr<FViewportClient> InViewportClient);
-   ~FMYViewport();
-    virtual void* GetWindow() override;
-    virtual void MoveWindow(int32 NewPosX, int32 NewPosY, int32 NewSizeX, int32 NewSizeY) override;
-    virtual void Destroy() override;
-    virtual bool SetUserFocus(bool bFocus) override;
-    virtual bool KeyState(FKey Key) const override;
-    virtual int32 GetMouseX() const override;
-    virtual int32 GetMouseY() const override;
-    virtual void GetMousePos(FIntPoint& MousePosition, const bool bLocalPosition = true) override;
-    virtual void SetMouse(int32 x, int32 y) override;
-    virtual void ProcessInput(float DeltaTime) override;
-    virtual FVector2D VirtualDesktopPixelToViewport(FIntPoint VirtualDesktopPointPx) const override;
-    virtual FIntPoint ViewportToVirtualDesktopPixel(FVector2D ViewportCoordinate) const override;
-    virtual void InvalidateDisplay() override;
-    virtual TObjectPtr<FViewportFrame> GetViewportFrame() override;
-
-};
-
-class FMYViewportClient : public FViewportClient
-{
-public:
-    FMYViewportClient() {};
-   ~FMYViewportClient() {};
-   TSharedPtr<FMYViewport> Viewport;
-
-};
-
-class FCamera : public ACameraActor
-{
-public:
-    FCamera() {};
-   ~FCamera() {};
-};
-
-class FCharacter : public ACharacter
-{
-public:
-    FCharacter() {};
-   ~FCharacter() {};
-
-};
-
-class FPlayerController : public APlayerController
-{
-public:
-    FPlayerController() {};
-   ~FPlayerController() {};
 };
