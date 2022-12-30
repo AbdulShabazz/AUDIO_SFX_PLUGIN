998ff434a8d9616340cfabd54095bce44e16dc65
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-30 11:51:48 -0500

    Update PinkNoise.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/PinkNoise.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/PinkNoise.cpp
@@ -9,13 +9,24 @@ PinkNoise::PinkNoise()
 // Apply 1/f^2 high-pass filter to shape noise spectrum
 void PinkNoise::ApplyPinkFilter(FILEINFO_Obj& FileInfoObj)
 {
-    int I = FileInfoObj.LengthInt16;
-    for (int i = 0; i < I; i++)
+    // Generate noise sample buffer
+    UE_UINT64 iUInt64 = 0;
+    for (UE_FLOAT64REF SampleIdxFloat64Ref : FileInfoObj.NoiseBufferFloat64)
     {
-        FileInfoObj.NoiseBufferInt32[i] /= std::sqrt(i*i);
+        SampleIdxFloat64Ref /= std::sqrt(iUInt64 * iUInt64 + 1);
     }
 }
 
+/**
+* Pink Noise is a type of noise that has a frequency
+* spectrum that is inversely proportional to the frequency
+* of the signal. This means that the power spectral density
+* (a measure of the energy of a signal at different frequencies)
+* of Pink Noise decreases by 3 dB per octave as the frequency increases.
+* @param [ FileInfoObj ] --- The file parameters struct
+* used to retain details for the file.
+* @return [ void ] --- No return value.
+*/
 void PinkNoise::GeneratePinkNoise(FILEINFO_Obj& FileInfoObj)
 {
     WhiteNoise WhiteNoiseObj;
