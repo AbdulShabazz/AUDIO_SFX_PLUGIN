859dd07b20476b2664470278a168d91ed021bbad
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2022-12-30 11:51:44 -0500

    Update WhiteNoise.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/WhiteNoise.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/WhiteNoise.cpp
@@ -4,15 +4,34 @@
 WhiteNoise::WhiteNoise()
 {
 }
-
+/**
+* White Noise is a type of noise that is characterized
+* by a flat frequency spectrum. It is a random, broadband
+* sound that contains equal energy per frequency and is
+* perceived as a constant, hissing sound. It is called
+* White Noise because it is analogous to white light,
+* which contains all frequencies of the visible spectrum.
+* @param [ FileInfoObj ] --- The file parameters struct
+* used to retain details for the file.
+* @return [ void ] --- No return value.
+*/
 void WhiteNoise::GenerateWhiteNoise(FILEINFO_Obj& FileInfoObj)
 {
+    // Seed the random number generator
     std::random_device rd;
+
+    // Set the range of the noise signal
     std::mt19937_64 gen(rd());
-    std::uniform_real_distribution<> dis(-TonesSettingsObj.AmplitudeInt16, TonesSettingsObj.AmplitudeInt16);
-    int I = FileInfoObj.LengthInt16;
-    for (int i = 0; i < I; i++)
+
+    // Generate noise sample buffer
+    std::uniform_real_distribution<UE_FLOAT64> dis(
+        -TonesSettingsObj.AmplitudeFloat64, 
+         TonesSettingsObj.AmplitudeFloat64
+        );
+
+    // Generate noise sample buffer
+    for (UE_FLOAT64REF SampleIdxFloat64Ref : FileInfoObj.NoiseBufferFloat64)
     {
-        FileInfoObj.NoiseBufferInt32[i] = dis(gen);
+        SampleIdxFloat64Ref = dis(gen);
     }
 }
