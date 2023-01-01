f7963a762208443c754999d49fd8574bc54e670d
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-01 05:36:35 -0500

    Update Tones.h

--- Source/AUDIO_SFX_PLUGIN/Public/Tones.h
+++ Source/AUDIO_SFX_PLUGIN/Public/Tones.h
@@ -1,39 +1,14 @@
 #pragma once
 
-class BlackNoise;
-class BlueNoise;
-class GreyNoise;
-class PinkNoise;
-class PurpleNoise;
-class WhiteNoise;
-class WhitePerlinNoise;
+#include "BlackNoise.h"
+#include "BlueNoise.h"
+#include "GreyNoise.h"
+#include "PinkNoise.h"
+#include "PurpleNoise.h"
+#include "WhiteNoise.h"
+#include "WhitePerlinNoise.h"
+#include "SimplexNoise.h"
+#include "FractalNoise.h"
 
-class Tone : public 
-    BlackNoise, 
-    BlueNoise,
-    GreyNoise,
-    PinkNoise,
-    PurpleNoise,
-    WhiteNoise,
-    WhitePerlinNoise
-{
+//ToneLibrary::BlackNoise;
 
-public:
-
-    Tone();
-
-    struct NOISE_OBJ
-    {
-        BlackNoise Black;
-        BlueNoise Blue;
-        GreyNoise Grey;
-        PinkNoise Pink;
-        PurpleNoise Purple;
-        WhiteNoise White;
-        WhitePerlinNoise Perlin;
-    } NoiseObj;
-
-    struct RANDOM_TONE_OBJ {
-
-    } RandomToneGenerateObj;
-};
