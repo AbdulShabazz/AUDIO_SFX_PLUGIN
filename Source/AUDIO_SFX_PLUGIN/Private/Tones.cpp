bf54f8fa55fdc472780a90c2bf5d1074c2b06759
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-01 05:35:20 -0500

    Update Tones.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/Tones.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/Tones.cpp
@@ -1,12 +1,2 @@
 #include "Tones.h"
-#include "BlackNoise.h"
-#include "BlueNoise.h"
-#include "GreyNoise.h"
-#include "PinkNoise.h"
-#include "PurpleNoise.h"
-#include "WhiteNoise.h"
-#include "WhitePerlinNoise.h"
 
-Tone::Tone()
-{
-}
