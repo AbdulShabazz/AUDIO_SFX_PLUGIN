2ae2bf1635e642f7793bba32ea185aebedfee76b
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-03 03:30:13 -0500

    Update AudioEqualization.h

--- Source/AUDIO_SFX_PLUGIN/Public/AudioEqualization.h
+++ Source/AUDIO_SFX_PLUGIN/Public/AudioEqualization.h
@@ -1,9 +1,13 @@
 #pragma once
 
+#include "Tones.Default.Settings.h"
+
 struct FILEINFO_Obj;
 
 namespace ToneLibrary 
 {
+    using ComplexVectorFloat64 = std::vector<std::complex<UE_FLOAT64>>;
+
     class AudioEqualization
     {
     public:
@@ -11,6 +15,16 @@ namespace ToneLibrary
         void GenerateAudioEqualization(FILEINFO_Obj& FileInfoObj);
     protected:
         void ApplyAudioEqualizationFilter(FILEINFO_Obj& FileInfoObj);
+        void CooleyTukeyFFT(
+            FILEINFO_Obj& FileInfoObj,
+            ComplexVectorFloat64& );
+        void BluesteinFFT(
+            FILEINFO_Obj& FileInfoObj,
+            ComplexVectorFloat64& );
+        void Radix2FFT(
+            FILEINFO_Obj& FileInfoObj,
+            ComplexVectorFloat64& );
+        void NormalizeAudio(ComplexVectorFloat64& m);
     };
 }
 
