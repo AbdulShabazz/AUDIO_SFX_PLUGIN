d97c4bb46e97b68df5bee7db6865db66ad47c5ff
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:30:20 -0500

    Update BlueNoise.h

--- Source/AUDIO_SFX_PLUGIN/Public/BlueNoise.h
+++ Source/AUDIO_SFX_PLUGIN/Public/BlueNoise.h
@@ -1,15 +1,17 @@
 #pragma once
 
+template<typename T, typename U>
 struct FILEINFO_Obj;
 
 namespace ToneLibrary
 {
+    template<typename T, typename U>
     class BlueNoise
     {
     public:
         BlueNoise();
-        void GenerateBlueNoise(FILEINFO_Obj& FileInfoObj);
+        void GenerateBlueNoise(FILEINFO_Obj<T,U>& FileInfoObj);
     protected:
-        void ApplyBlueFilter(FILEINFO_Obj& FileInfoObj);
+        void ApplyBlueFilter(FILEINFO_Obj<T,U>& FileInfoObj);
     };
 }
