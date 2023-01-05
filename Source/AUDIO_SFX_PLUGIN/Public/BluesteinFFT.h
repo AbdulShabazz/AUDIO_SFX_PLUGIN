b6cb57dcaa99e9e2b43a4f0380c2509f44299da8
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:33:53 -0500

    Update BluesteinFFT.h

--- Source/AUDIO_SFX_PLUGIN/Public/BluesteinFFT.h
+++ Source/AUDIO_SFX_PLUGIN/Public/BluesteinFFT.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include "Tones.Default.Settings.h"
+
 namespace ToneLibrary
 {
+    template <typename T,typename U>
     class BluesteinFFT
     {
+    public:
+        BluesteinFFT();
+        void GenerateBluesteinFFT(
+            FILEINFO_Obj<T, U>& FileInfoObjRef
+            );
+    private:
+        void ApplyBluesteinFFTFilter(
+            FILEINFO_Obj<T, U>& FileInfoObjRef
+            );
     };
 }
 
