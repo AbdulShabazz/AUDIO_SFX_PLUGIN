ea767a6eb86a940a022304f15bca42932bffcb5d
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:30:38 -0500

    Update BlackNoise.h

--- Source/AUDIO_SFX_PLUGIN/Public/BlackNoise.h
+++ Source/AUDIO_SFX_PLUGIN/Public/BlackNoise.h
@@ -1,15 +1,15 @@
 #pragma once
-
-struct FILEINFO_Obj;
+#include "Tones.Default.Settings.h"
 
 namespace ToneLibrary
 {
+    template<typename T, typename U>
     class BlackNoise
     {
     public:
         BlackNoise();
-        void GenerateBlackNoise(FILEINFO_Obj& FileInfoObj);
+        void GenerateBlackNoise(FILEINFO_Obj<T,U>& FileInfoObj);
     protected:
-        void ApplyBlackFilter(FILEINFO_Obj& FileInfoObj);
+        void ApplyBlackFilter(FILEINFO_Obj<T,U>& FileInfoObj);
     };
 }
