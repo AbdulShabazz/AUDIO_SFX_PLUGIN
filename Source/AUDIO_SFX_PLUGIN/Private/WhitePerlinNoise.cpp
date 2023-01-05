be99b5b33f6c9b21b91a6880390a254df8a4b4e8
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:31:02 -0500

    Update WhitePerlinNoise.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/WhitePerlinNoise.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/WhitePerlinNoise.cpp
@@ -3,11 +3,13 @@
 
 using namespace ToneLibrary;
 
-WhitePerlinNoise::WhitePerlinNoise()
+template<typename T, typename U>
+WhitePerlinNoise<T,U>::WhitePerlinNoise()
 {
 }
 
-void WhitePerlinNoise::ApplyWhitePerlinNoiseFilter(FILEINFO_Obj& FileInfoObj)
+template<typename T, typename U>
+void WhitePerlinNoise<T,U>::ApplyWhitePerlinNoiseFilter(FILEINFO_Obj<T,U>& FileInfoObj)
 {
     // Seed the random number generator
     std::random_device rd;
@@ -16,54 +18,55 @@ void WhitePerlinNoise::ApplyWhitePerlinNoiseFilter(FILEINFO_Obj& FileInfoObj)
     std::mt19937_64 gen(rd());
 
     // Set the range of the noise signal
-    UniformRealDistributionFloat64 dis(-1.0f, 1.0f);
+    std::uniform_real_distribution<T> dis(-1.0f, 1.0f);
 
     // Init a vec array of gradients
-    std::vector<std::vector<UE_FLOAT64>> Gradients2DFloat64(2, std::vector<UE_FLOAT64>(FileInfoObj.PerlinNoiseNumGradientsFloat64));
+    Vector2DT<T> Gradients2DFloat64(2, VectorT<T>(FileInfoObj.PerlinNoiseNumGradientsFloat64));
 
     // Generate a set of random gradients
-    for(UE_UINT64 j = 0; j < FileInfoObj.PerlinNoiseNumGradientsFloat64; j++)
+    for(U j = 0; j < FileInfoObj.PerlinNoiseNumGradientsFloat64; j++)
     {
         Gradients2DFloat64[0][j] = dis(gen);
         Gradients2DFloat64[1][j] = dis(gen);
     }
 
     // Generate noise data
-    std::vector<UE_FLOAT64> NoiseMap1DFloat64(
-        FileInfoObj.PerlinNoiseWidthUInt64 * FileInfoObj.PerlinNoiseHeightUInt64
+    VectorT<T> NoiseMap1DFloat64(
+        FileInfoObj.PerlinNoiseWidthUInt64 * 
+        FileInfoObj.PerlinNoiseHeightUInt64
         );
-    UE_UINT64 hUint64 = FileInfoObj.PerlinNoiseHeightUInt64;
-    UE_UINT64 wUint64 = FileInfoObj.PerlinNoiseWidthUInt64;
-    for (UE_UINT64 i = 0; i < hUint64; i++)
+    U hUint64 = FileInfoObj.PerlinNoiseHeightUInt64;
+    U wUint64 = FileInfoObj.PerlinNoiseWidthUInt64;
+    for (U i = 0; i < hUint64; i++)
     {
-        for (UE_UINT64 j = 0; j < wUint64; j++)
+        for (U j = 0; j < wUint64; j++)
         {
             // Calculate the indices of the four gradients that enclose the point
-            UE_UINT64 i0 = i / 2;
-            UE_UINT64 j0 = j / 2;
-            UE_UINT64 i1 = i0 + 1;
-            UE_UINT64 j1 = j0 + 1;
+            U i0 = static_cast<U>(i / 2);
+            U j0 = static_cast<U>(j / 2);
+            U i1 = i0 + 1;
+            U j1 = j0 + 1;
 
             // Calculate the weight for each gradient
-            UE_FLOAT64 wi = i / 2.0f - i0;
-            UE_FLOAT64 wj = j / 2.0f - j0;
+            T wi = i / 2.0f - i0;
+            T wj = j / 2.0f - j0;
 
             // Interpolate the values at the four (4) gradients
-            UE_FLOAT64 n0 = DotProductFloat64(
+            T n0 = DotProductFloat64(
                 Gradients2DFloat64[i0 + wUint64 + j0], { wi, wj }
                 );
-            UE_FLOAT64 n1 = DotProductFloat64(
+            T n1 = DotProductFloat64(
                 Gradients2DFloat64[i1 + wUint64 + j0], { wi, wj }
                 );
-            UE_FLOAT64 n2 = DotProductFloat64(
+            T n2 = DotProductFloat64(
                 Gradients2DFloat64[i0 + wUint64 + j1], { wi, wj }
                 );
-            UE_FLOAT64 n3 = DotProductFloat64(
+            T n3 = DotProductFloat64(
                 Gradients2DFloat64[i1 + wUint64 + j1], { wi, wj }
                 );
 
             // Interpolate between the four values to get the final noise value for the point
-            UE_FLOAT64 NoiseValueFloat64 = BilinearInterpolationT<UE_FLOAT64>(n0, n1, n2, n3, wi, wj);
+            T NoiseValueFloat64 = BilinearInterpolationT<T>(n0, n1, n2, n3, wi, wj);
 
             // Store the NoiseValueFloat64 in the noise map
             NoiseMap1DFloat64[i + wUint64 * j] = NoiseValueFloat64;
@@ -81,7 +84,8 @@ void WhitePerlinNoise::ApplyWhitePerlinNoiseFilter(FILEINFO_Obj& FileInfoObj)
 * used to retain details for the file.
 * @return [ void ] --- No return value.
 */
-void WhitePerlinNoise::GenerateWhitePerlinNoise(FILEINFO_Obj& FileInfoObj)
+template<typename T, typename U>
+void WhitePerlinNoise<T,U>::GenerateWhitePerlinNoise(FILEINFO_Obj<T,U>& FileInfoObj)
 {
     ApplyWhitePerlinNoiseFilter(FileInfoObj);
 }
