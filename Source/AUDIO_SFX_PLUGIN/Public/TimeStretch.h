375aa88eb552cc11618ec08f821669337fc9f454
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:31:13 -0500

    Update TimeStretch.h

--- Source/AUDIO_SFX_PLUGIN/Public/TimeStretch.h
+++ Source/AUDIO_SFX_PLUGIN/Public/TimeStretch.h
@@ -1,16 +1,18 @@
 #pragma once
 
+template<typename T, typename U>
 struct FILEINFO_Obj;
 
 namespace ToneLibrary
 {
-        class TimeStretch
+    template<typename T, typename U>
+    class TimeStretch
     {
     public:
         TimeStretch();
-        void GenerateTimeStretch(FILEINFO_Obj& FileInfoObj);
+        void GenerateTimeStretch(FILEINFO_Obj<T,U>& FileInfoObj);
     protected:
-        void ApplyTimeStretchFilter(FILEINFO_Obj& FileInfoObj);
+        void ApplyTimeStretchFilter(FILEINFO_Obj<T,U>& FileInfoObj);
     };
 }
 
