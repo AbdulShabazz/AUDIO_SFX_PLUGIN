acf0d2046d1ff50dad1efd600af8b43310ade000
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:32:57 -0500

    Update PurpleNoise.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/PurpleNoise.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/PurpleNoise.cpp
@@ -4,23 +4,26 @@
 
 using namespace ToneLibrary;
 
-PurpleNoise::PurpleNoise()
+template <typename T, typename U>
+PurpleNoise<T,U>::PurpleNoise()
 {
 }
 
-void PurpleNoise::ApplyPurpleFilter(FILEINFO_Obj& FileInfoObj)
+template <typename T, typename U>
+void PurpleNoise<T,U>::ApplyPurpleFilter(FILEINFO_Obj<T,U>& FileInfoObj)
 {
-    UE_UINT64 iUInt64 = 0;
-    for (UE_FLOAT64REF SampleIdxFloat64Ref : FileInfoObj.NoiseBufferFloat64)
+    U iUInt64 = 0;
+    for (T& SampleIdxFloat64Ref : FileInfoObj.NoiseBufferFloat64)
     {
         SampleIdxFloat64Ref /= std::sqrt(iUInt64++ + 1);
     }
 }
 
 // Apply 1/f high-pass filter to shape noise spectrum
-void PurpleNoise::GeneratePurpleNoise(FILEINFO_Obj& FileInfoObj)
+template <typename T, typename U>
+void PurpleNoise<T,U>::GeneratePurpleNoise(FILEINFO_Obj<T,U>& FileInfoObj)
 {
-    WhiteNoise WhiteNoiseObj;
+    WhiteNoise<T> WhiteNoiseObj;
     WhiteNoiseObj.GenerateWhiteNoise(FileInfoObj);
     ApplyPurpleFilter(FileInfoObj);
 }
