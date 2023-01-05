1b5f4ba6bd60eda9089bab286742fef7f64779ff
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:33:24 -0500

    Update GreyNoise.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/GreyNoise.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/GreyNoise.cpp
@@ -3,7 +3,8 @@
 
 using namespace ToneLibrary;
 
-GreyNoise::GreyNoise()
+template<typename T, typename U>
+GreyNoise<T, U>::GreyNoise()
 {
 }
 
@@ -15,12 +16,14 @@ GreyNoise::GreyNoise()
 * used to retain details for the file.
 * @return [ void ] --- No return value.
 */
-void GreyNoise::GenerateGreyNoise(FILEINFO_Obj& FileInfoObj)
+template<typename T, typename U>
+void GreyNoise<T,U>::GenerateGreyNoise(FILEINFO_Obj<T,U>& FileInfoObj)
 {
     ApplyGreyFilter(FileInfoObj);
 }
 
-void GreyNoise::ApplyGreyFilter(FILEINFO_Obj& FileInfoObj)
+template<typename T, typename U>
+void GreyNoise<T,U>::ApplyGreyFilter(FILEINFO_Obj<T,U>& FileInfoObj)
 {
     UE_FLOAT64 mUInt64 = 1ull << 31;
     constexpr double powerInt64 = -1.0;
@@ -32,14 +35,14 @@ void GreyNoise::ApplyGreyFilter(FILEINFO_Obj& FileInfoObj)
     std::mt19937_64 gen(rd());
 
     // Generate noise sample buffer
-    std::uniform_real_distribution<UE_FLOAT64> dis(
+    std::uniform_real_distribution<T> dis(
         -TonesSettingsObj.AmplitudeFloat64, 
          TonesSettingsObj.AmplitudeFloat64
         );
 
     // Generate noise sample buffer
-    for (UE_FLOAT64REF SampleIdxFloat64Ref : FileInfoObj.NoiseBufferFloat64)
+    for (T SampleIdxTRef : FileInfoObj.NoiseBufferFloat64)
     {
-        SampleIdxFloat64Ref = std::sqrt(std::pow(dis(gen) / static_cast<double>(mUInt64), powerInt64));
+        SampleIdxTRef = std::sqrt(std::pow(dis(gen) / static_cast<T>(mUInt64), powerInt64));
     }
 }
