b3f63e8869ce65a657b48ba15869b65c2f3c2bf7
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:33:12 -0500

    Update PinkNoise.h

--- Source/AUDIO_SFX_PLUGIN/Public/PinkNoise.h
+++ Source/AUDIO_SFX_PLUGIN/Public/PinkNoise.h
@@ -1,15 +1,17 @@
 #pragma once
 
+template<typename T, typename U>
 struct FILEINFO_Obj;
 
 namespace ToneLibrary
 {
+    template<typename T, typename U>
     class PinkNoise
     {
     public:
         PinkNoise();
-        void GeneratePinkNoise(FILEINFO_Obj& FileInfoObj);
+        void GeneratePinkNoise(FILEINFO_Obj<T,U>& FileInfoObj);
     protected:
-        void ApplyPinkFilter(FILEINFO_Obj& FileInfoObj);
+        void ApplyPinkFilter(FILEINFO_Obj<T,U>& FileInfoObj);
     };
 }
