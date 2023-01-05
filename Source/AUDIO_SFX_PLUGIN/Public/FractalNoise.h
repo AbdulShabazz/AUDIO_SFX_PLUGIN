c7ce2f53fcf7ad36f25f1dee4d32faaabaeeaf9c
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:33:28 -0500

    Update FractalNoise.h

--- Source/AUDIO_SFX_PLUGIN/Public/FractalNoise.h
+++ Source/AUDIO_SFX_PLUGIN/Public/FractalNoise.h
@@ -1,21 +1,23 @@
 #pragma once
 
+template<typename T, typename U>
 struct FILEINFO_Obj;
 
 namespace ToneLibrary
 {
+    template<typename T, typename U>
     class FractalNoise
     {
     protected:
-        void ApplyDiamondNoiseArrayShape(FILEINFO_Obj& FileInfoObj);
-        void ApplyTriangularNoiseArrayShape(FILEINFO_Obj& FileInfoObj);
-        void ApplyCircularNoiseArrayShape(FILEINFO_Obj& FileInfoObj);
-        void ApplyRectangularNoiseArrayShape(FILEINFO_Obj& FileInfoObj);
-        void ApplySquareNoiseArrayShape(FILEINFO_Obj& FileInfoObj);
-        void ApplyFractalNoiseFilter(FILEINFO_Obj& FileInfoObj);
+        void ApplyDiamondNoiseArrayShape(FILEINFO_Obj<T,U>& FileInfoObj);
+        void ApplyTriangularNoiseArrayShape(FILEINFO_Obj<T,U>& FileInfoObj);
+        void ApplyCircularNoiseArrayShape(FILEINFO_Obj<T,U>& FileInfoObj);
+        void ApplyRectangularNoiseArrayShape(FILEINFO_Obj<T,U>& FileInfoObj);
+        void ApplySquareNoiseArrayShape(FILEINFO_Obj<T,U>& FileInfoObj);
+        void ApplyFractalNoiseFilter(FILEINFO_Obj<T,U>& FileInfoObj);
     public:
         FractalNoise();
-        void GenerateFractalNoise(FILEINFO_Obj& FileInfoObj);
+        void GenerateFractalNoise(FILEINFO_Obj<T,U>& FileInfoObj);
     };
 
 }
