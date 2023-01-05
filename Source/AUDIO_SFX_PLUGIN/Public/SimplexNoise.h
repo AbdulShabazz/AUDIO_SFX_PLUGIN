44dfa751931fb97bf037d640a2f6d3f85c02b9fa
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-05 05:32:28 -0500

    Update SimplexNoise.h

--- Source/AUDIO_SFX_PLUGIN/Public/SimplexNoise.h
+++ Source/AUDIO_SFX_PLUGIN/Public/SimplexNoise.h
@@ -1,16 +1,18 @@
 #pragma once
 
+template<typename T, typename U>
 struct FILEINFO_Obj;
 
 namespace ToneLibrary
 {
+    template<typename T, typename U>
     class SimplexNoise
     {
     public:
         SimplexNoise();
-        void GenerateSimplexNoise(FILEINFO_Obj& FileInfoObj);
+        void GenerateSimplexNoise(FILEINFO_Obj<T,U>& FileInfoObj);
     protected:
-        void ApplySimplexNoiseFilter(FILEINFO_Obj& FileInfoObj);
+        void ApplySimplexNoiseFilter(FILEINFO_Obj<T,U>& FileInfoObj);
     };
 }
 
