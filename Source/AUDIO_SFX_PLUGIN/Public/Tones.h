38bf5ad8fcd01603eacde35d556ff949dad5d9c0
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-08 01:41:35 -0500

    Update Tones.h

--- Source/AUDIO_SFX_PLUGIN/Public/Tones.h
+++ Source/AUDIO_SFX_PLUGIN/Public/Tones.h
@@ -1,14 +1,27 @@
 #pragma once
 
+#include "AudioEqualization.h"
+#include "AudioMix.h"
+#include "AudioSumation.h"
 #include "BlackNoise.h"
 #include "BlueNoise.h"
+#include "BluesteinFFT.h"
+#include "ConvolutionReverb.h"
+#include "CooleyTukeyFFT.h"
+#include "FractalNoise.h"
 #include "GreyNoise.h"
+#include "KarplusStrongDelayLineSynthesis.h"
+#include "MIDI.h"
 #include "PinkNoise.h"
+#include "PlateReverb.h"
 #include "PurpleNoise.h"
+#include "Radix2FFT.h"
+#include "SimplexNoise.h"
+#include "SpringReverb.h"
+#include "TimeStretch.h"
+#include "WaveEquation.h"
 #include "WhiteNoise.h"
 #include "WhitePerlinNoise.h"
-#include "SimplexNoise.h"
-#include "FractalNoise.h"
 
 //ToneLibrary::BlackNoise;
 
