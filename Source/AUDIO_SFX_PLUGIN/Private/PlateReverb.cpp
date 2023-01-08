265d41e82934cb0d468e4b1bb1c682d2ea527698
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-08 01:40:42 -0500

    Update PlateReverb.cpp

--- Source/AUDIO_SFX_PLUGIN/Private/PlateReverb.cpp
+++ Source/AUDIO_SFX_PLUGIN/Private/PlateReverb.cpp
@@ -2,8 +2,58 @@
 
 using namespace ToneLibrary;
 
-template<typename T>
-PlateReverb<T>::PlateReverb()
+template<typename T, typename U>
+PlateReverb<T,U>::PlateReverb()
 {
 
+}
+
+template<typename T, typename U>
+void PlateReverb<T, U>::ApplyPlateReverbFilter(
+	FILEINFO_Obj<T, U>& FileInfoObj_Ref
+	)
+{
+	
+}
+
+template<typename T, typename U>
+void PlateReverb<T, U>::GeneratePlateReverb(
+	FILEINFO_Obj<T, U>& FileInfoObj_Ref
+	)
+{
+	// PlateReverb
+}
+
+template<typename T, typename U>
+void PlateReverb<T, U>::SetSize(
+	FILEINFO_Obj<T, U>& FileInfoObj_Ref
+	)
+{
+	// Set size of reverb
+	// U Size
+	//Size = 0.5;
+}
+
+template<typename T, typename U>
+void PlateReverb<T, U>::SetDamping(
+	FILEINFO_Obj<T, U>& FileInfoObj_Ref
+	)
+{	
+	//T Damping
+}
+
+template<typename T, typename U>
+void PlateReverb<T, U>::SetWetLevel(
+	FILEINFO_Obj<T, U>& FileInfoObj_Ref
+	)
+{
+	//T WetLevel
+}
+
+template<typename T, typename U>
+void PlateReverb<T, U>::SetDryLevel(
+	FILEINFO_Obj<T, U>& FileInfoObj_Ref
+	)
+{
+	//T DryLevel
 }
