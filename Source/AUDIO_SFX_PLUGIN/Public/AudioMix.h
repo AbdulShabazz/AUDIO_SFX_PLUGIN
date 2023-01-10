4624cc64316d76828b6850d60e1c999439a3dd3b
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-10 01:45:17 -0500

    Update AudioMix.h

--- Source/AUDIO_SFX_PLUGIN/Public/AudioMix.h
+++ Source/AUDIO_SFX_PLUGIN/Public/AudioMix.h
@@ -1,8 +1,16 @@
 #pragma once
+
+#include "Tones.Default.Settings.h"
+
 namespace ToneLibrary
 {
+    template<typename T, typename U>
     class AudioMix
     {
+    public:
+        AudioMix();
+        void GenerateAudioMix(FILEINFO_Obj<T, U>&);
+        void ResetAudioMixResult(FILEINFO_Obj<T, U>&);
     };
 }
 
