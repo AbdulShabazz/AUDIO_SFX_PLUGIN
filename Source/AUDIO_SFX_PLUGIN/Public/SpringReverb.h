b4cfc9a0ae00624d69c68db1c2669e0296562770
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-10 01:46:11 -0500

    Update SpringReverb.h

--- Source/AUDIO_SFX_PLUGIN/Public/SpringReverb.h
+++ Source/AUDIO_SFX_PLUGIN/Public/SpringReverb.h
@@ -1,10 +1,15 @@
 #pragma once
 
+#include "Tones.Default.Settings.h"
+
 namespace ToneLibrary
 {
-    template<typename T>
+    template<typename T, typename U>
     class SpringReverb
     {
+    public:
+        SpringReverb();
+        void GenerateSpringReverb(FILEINFO_Obj<T, U>&);
     };
 }
 
