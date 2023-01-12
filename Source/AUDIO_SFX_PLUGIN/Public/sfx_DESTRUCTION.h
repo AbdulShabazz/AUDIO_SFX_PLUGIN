04d78b7c16d4e99acb24925159b7d5a721886a3f
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-12 07:44:09 -0500

    Create sfx_DESTRUCTION.h

---
+++ Source/AUDIO_SFX_PLUGIN/Public/sfx_DESTRUCTION.h
@@ -0,0 +1,15 @@
+
+#pragma once
+
+#include "Tones.Default.Settings.h"
+
+namespace ToneLibrary
+{
+
+	template<typename T, typename U>
+	class sfx_DESTRUCTION
+	{
+	public:
+		sfx_DESTRUCTION();
+	};
+}
