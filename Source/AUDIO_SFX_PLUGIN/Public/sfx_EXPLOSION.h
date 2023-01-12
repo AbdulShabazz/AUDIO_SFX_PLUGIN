4c6372ef008fb8c19c08bd1d0a8bca2368f70b3c
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-12 07:44:15 -0500

    Create sfx_EXPLOSION.h

---
+++ Source/AUDIO_SFX_PLUGIN/Public/sfx_EXPLOSION.h
@@ -0,0 +1,15 @@
+
+#pragma once
+
+#include "Tones.Default.Settings.h"
+
+namespace ToneLibrary
+{
+
+	template<typename T, typename U>
+	class sfx_EXPLOSION
+	{
+	public:
+		sfx_EXPLOSION();
+	};
+}
