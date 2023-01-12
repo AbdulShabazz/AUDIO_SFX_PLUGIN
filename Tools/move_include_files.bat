c6ff36c02b003ea1110f6373d089c242311f6d6d
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-12 07:27:52 -0500

    Create move_include_files.bat

---
+++ Tools/move_include_files.bat
@@ -0,0 +1,4 @@
+echo Moving class declarations to public folder
+move *.h ../Source/AUDIO_SFX_PLUGIN/Public
+echo Moving class definitions to private folder
+move *.cpp ../Source/AUDIO_SFX_PLUGIN/Private
