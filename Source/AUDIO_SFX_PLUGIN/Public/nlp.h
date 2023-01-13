b18d833d3390c768775ef3cd57564f094c12bd81
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-13 01:59:02 -0500

    Update nlp.h

--- Source/AUDIO_SFX_PLUGIN/Public/nlp.h
+++ Source/AUDIO_SFX_PLUGIN/Public/nlp.h
@@ -7,6 +7,14 @@
 namespace ToneLibrary
 {
 
+	/**
+	* Natural Language Processing (NLP) Text-to-Speech (TTS) pipeline for a Text-for-Synthesis (TFS) prompt : 
+	* Prompt ---> [Phoneme CODEC] <---> [Neural Language Model CODEC] <---> [Audio Synthesis CODEC] ---> Personalized Speech. 
+	* [Audio CODEC] is a 39 parallel concatenative additive voice synthesis engine with builtin breathe dynamics, Multisyn
+	* via standard unit selection concatenative methods
+	* @param [] ---
+	* @returns [] ---
+	*/
 	template<typename T, typename U>
 	class nlp
 	{
@@ -15,7 +23,7 @@ namespace ToneLibrary
 
 		template<typename T>
 		static std::unordered_map<std::string, 
-		std::vector<std::vector<std::complex<T>>>> nlp_has_phoneme;
+		std::vector<std::vector<std::complex<T>>>> nlp_phoneme;
 	};
 
 }
