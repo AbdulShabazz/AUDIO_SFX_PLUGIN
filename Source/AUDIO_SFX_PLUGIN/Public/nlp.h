a3a2e4598e88043f76b8e4921d206f3bb9f90d48
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-14 11:29:40 -0500

    Update nlp.h

--- Source/AUDIO_SFX_PLUGIN/Public/nlp.h
+++ Source/AUDIO_SFX_PLUGIN/Public/nlp.h
@@ -20,6 +20,8 @@ namespace ToneLibrary
 	{
 	public:
 		nlp();
+		void nlp_ParallelHiddenMarkov_tts();
+		void nlp_SerialHiddenMarkov_tts();
 
 		template<typename T>
 		static std::unordered_map<std::string, 
