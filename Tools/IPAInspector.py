8c5ce0ea282a1a41b179b3ca8ea632ee4b006007
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-18 09:18:56 -0500

    Create IPAInspector.py

---
+++ Tools/IPAInspector.py
@@ -0,0 +1,43 @@
+# import IPADictionary # Ex 'IPADictionary.Consonant["ch"] # (True)', 'IPADictionary.Vowel["a"] # (True)'
+import re
+import json
+import threading
+from string import Template
+import IPASpellToSpeech # Ex IPASpellToSpeech["u"] = "oo" >> "m_a_s_a_ch_oo_s_e_t_s"
+
+MAXTHREADS = 18
+
+vowel = {
+    "a":True,
+    "e":True,
+    "i":True,
+    "o":True,
+    "u":True
+}
+
+pattern = r'([bcdfghjklmnpqrstvwxyz]*[aeiou]*[bcdfghjklmnpqrstvwxyz]*)'
+syllable = {}
+
+def SyllableInspector(word):
+    try:
+        for phonemeGroup in re.findall(pattern, word):
+            if phonemeGroup and not syllable.get(phonemeGroup):
+                syllable[phonemeGroup] = True
+    except Exception as e:
+        pass # print(e)
+
+with open("database.corpus.generated.log","r") as f:
+    worker = []
+    for line in f.readlines():
+        if not re.search("^#", line):
+            word = re.sub("[\'\.\n]+", "", line)
+            for wd in word.split("_"):
+                th = threading.Thread(target=SyllableInspector, args=(wd,))
+                th.start()
+                worker += [th]
+    for th in worker:
+        th.join()
+    with open("syllable.corpus.generated.log","w") as g:
+        g.writelines(json.dumps(syllable, sort_keys=True, indent=2))
+
+print("Q.E.D.")
