8c9781df99852f57edecadacb3abb72561c57b3a
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-19 00:22:23 -0500

    Update IPADictionary.py

--- Tools/IPADictionary.py
+++ Tools/IPADictionary.py
@@ -1,54 +1,92 @@
 IPADictionary = {
-    "Consonant":
-    {
-        "b": True, # as in "bat"
-        "d": True, # as in "dog"
-        "f": True, # as in "fish"
-        "g": True, # as in "go"
-        "h": True, # as in "hat"
-        "j": True, # as in "jump, budget"
-        "k": True, # as in "cat"
-        "l": True, # as in "lip"
-        "m": True, # as in "mat"
-        "n": True, # as in "nap"
-        "ng": True, # as in "sing"
-        "p": True, # as in "pat"
-        "r": True, # as in "rip"
-        "s": True, # as in "sip"
-        "sh": True, # as in "sheep"
-        "t": True, # as in "tap"
-        "ch": True, # as in "church, witch"
-        "th": True, # as in "think, math, nothing"
-        "th2": True, # as in "this, breathe"
-        "v": True, # as in "vat"
-        "w": True, # as in "wet"
-        "hw": True, # as in "where, somewhat"
-        "y": True, # as in "yacht, yes, onion"
-        "z": True, # as in "zoo, zip, easy"
-        "z2": True # as in "measure, television, beige"
-    },
-
-    "Vowel":
-    {
-        "a": True, # as in "apple, hat, bat"
-        "ey": True, # as in "aid, bait, day"
-        "ah": True, # as in "arm, father, aha"
-        "air": True, # as in "air, careful, wear"
-        "aw": True, # as in "bought"
-        "e": True, # as in "bet, ever, head"
-        "ee": True, # as in "beet, eat, seed, need"
-        "eer": True, # as in "teacher, murderer"
-        "i": True, # as in "it, finishes"
-        "ahy": True, # as in "I, ice, deny"
-        "o": True, # as in "odd, waffle"
-        "oh": True, # as in "owe, road"
-        "oo": True, # as in "rude, soup"
-        "oo2": True, # as in "good, book"
-        "ou": True, # as in "out, how, loud"
-        "uh": True, # as in "up, mother"
-        "uh2": True, # as in "about, problem, circus"
-        "ur": True # as in "early, stirring"
-    }
+    #ordinal
+    "1st": True,
+    "2nd": True,
+    "3rd": True,
+    "Nth": True,
+    #Number
+    "0": True,
+    "1": True,
+    "2": True,
+    "3": True,
+    "4": True,
+    "5": True,
+    "6": True,
+    "7": True,
+    "8": True,
+    "9": True,
+    #Letter
+    "A": True,
+    "B": True,
+    "C": True,
+    "D": True,
+    "E": True,
+    "F": True,
+    "G": True,
+    "H": True,
+    "I": True,
+    "J": True,
+    "K": True,
+    "L": True,
+    "M": True,
+    "N": True,
+    "O": True,
+    "P": True,
+    "Q": True,
+    "R": True,
+    "S": True,
+    "T": True,
+    "U": True,
+    "V": True,
+    "W": True,
+    "X": True,
+    "Y": True,
+    "Z": True,
+    #Consonant
+    "b": True, # as in "bat"
+    "d": True, # as in "dog"
+    "f": True, # as in "fish"
+    "g": True, # as in "go"
+    "h": True, # as in "hat"
+    "j": True, # as in "jump, budget"
+    "k": True, # as in "cat"
+    "l": True, # as in "lip"
+    "m": True, # as in "mat"
+    "n": True, # as in "nap"
+    "ng": True, # as in "sing"
+    "p": True, # as in "pat"
+    "r": True, # as in "rip"
+    "s": True, # as in "sip"
+    "sh": True, # as in "sheep"
+    "t": True, # as in "tap"
+    "ch": True, # as in "church, witch"
+    "th": True, # as in "think, math, nothing"
+    "th2": True, # as in "this, breathe"
+    "v": True, # as in "vat"
+    "w": True, # as in "wet"
+    "hw": True, # as in "where, somewhat"
+    "y": True, # as in "yacht, yes, onion"
+    "z": True, # as in "zoo, zip, easy"
+    "z2": True, # as in "measure, television, beige"
+    #"Vowel":
+    "a": True, # as in "apple, hat, bat"
+    "ey": True, # as in "aid, bait, day"
+    "ah": True, # as in "arm, father, aha"
+    "air": True, # as in "air, careful, wear"
+    "aw": True, # as in "bought"
+    "e": True, # as in "bet, ever, head"
+    "ee": True, # as in "beet, eat, seed, need"
+    "eer": True, # as in "teacher, murderer"
+    "i": True, # as in "it, finishes"
+    "ahy": True, # as in "I, ice, deny"
+    "o": True, # as in "odd, waffle"
+    "oh": True, # as in "owe, road"
+    "oo": True, # as in "rude, soup"
+    "oo2": True, # as in "good, book"
+    "ou": True, # as in "out, how, loud"
+    "uh": True, # as in "up, mother"
+    "uh2": True, # as in "about, problem, circus"
+    "ur": True # as in "early, stirring"
 }
 
 # Ex 'IPADictionary.Consonant["ch"] # (True)', 'IPADictionary.Vowel["a"] # (True)'
