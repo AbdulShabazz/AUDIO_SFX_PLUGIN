0e073e59c1a49173e00ee6452633e14ac225079a
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-21 05:13:19 -0500

    Update IPATagger.chatgpt.openai.version.3.py

--- Tools/IPATagger.chatgpt.openai.version.3.py
+++ Tools/IPATagger.chatgpt.openai.version.3.py
@@ -6,6 +6,7 @@ import openai
 import threading
 
 archive = {}
+completed_archive = {}
 todo_archive = {}
 
 def populate_todo_archive(_):
@@ -15,15 +16,19 @@ def populate_todo_archive(_):
         for wd in f.readlines():
             if not resume_ready and re.search("^"+RESUME_KEYWORD, wd):
                 resume_ready = True
-            if resume_ready:
+            if resume_ready and not re.search("^#", wd):
                 wd = re.sub("[\'\.]+", "", wd)
                 todo_archive[wd] = True
 
-def populate_archive(_):
-    with open("pronounciation.corpus.generated.1674275980.4888992.log","r") as g:
+def populate_archive(_): # the latest archive
+    with open("pronounciation.corpus.generated.1674285811714235900.log","r") as g:
         for wd in g.readlines():
-            wd = re.sub("[_]+", "", wd)
-            archive[wd] = True
+            wd = wd.lower()
+            completed_archive[wd] = True
+            word = re.sub("[_]+", "", wd)
+            if word <> wd and word in completed_archive: # remove unformatted dups
+                completed_archive.pop(word)
+            archive[word] = True
 
 th_0000 = threading.Thread(target=populate_todo_archive, args=("",))
 th_0001 = threading.Thread(target=populate_archive, args=("",))
@@ -42,11 +47,11 @@ with open("pronounciation.corpus.todo.generated.log","w") as f:
             todos += [word]
             f.writelines(word) # preserve newlines
 
-myprompt = '''(1) Seperate each word by a newline, and (2) split each word into syllables, separated by underscores: '''
+myprompt = '''(1) Seperate each word by a newline, and (2) split each word into syllables -- separated by underscores: '''
 
 # OPENAI
 # @param - prompt [prompt.length + response.length < max_tokens]
-# @param - engine [code/text-davinci-ver, code/text-curie-ver, code/text-babbage-ver, code/text-ada-ver]
+# @param - engine [code/text]-[davinci/curie/babbage/ada]-[ver]
 # @param - max_tokens [prompt + response < 4097] Length. Bandwidth alloted for the session/connection 
 # @param - temperature [1.0 - 0.0] Strictness. Controls the creativity or randomness of the response
 # @param - top_p [1.0 - 0.0] Predictability. Controls the constrained randomness of oncomming text
@@ -54,25 +59,33 @@ myprompt = '''(1) Seperate each word by a newline, and (2) split each word into 
 # @param - presence_penalty [1.0 - 0.0] DisAllowed repetitiveness/Use synonyms. Ratio of input tokens allowed in the response
 # @returns  - { choices[{ engine:[davinci, curie], finish_reason:[stop,length], index:N, logprob:[], text:[response]},] }
 def issueQuery(word,g):
-    #time.sleep(1.1)
+    time.sleep(1.2)
     openai.api_key = os.getenv("OPENAI_API_KEY") # Set before callig this module
     response = openai.Completion.create(model="text-davinci-003", max_tokens=1600, presence_penalty=0.0, top_p=1.0, temperature = 1.0, prompt=myprompt + json.dumps(word) + "?")
     for choice in response.choices:
         #result = word + " = " + choice.text
-        g.writelines(choice.text + "\n")
+        g.writelines(choice.text.lower() + "\n")
         #print(choice.text)
 
 MAX_TOKENS = 4097 # total syllables = prompt + completions
 MAX_SEND_TOKENS = 425
+MAX_THREADS = 18
+SINGLE_THREADED = False
 
-with open("pronounciation.corpus.generated." + str(time.time_ns()) + ".log","a") as g:
+with open("pronounciation.corpus.generated." + str(time.time_ns()) + ".log","w") as g:
+    alltasks = []
     worker = []
     for word in todos:
         try:
             if len(worker) < MAX_SEND_TOKENS:
                 worker += [re.sub("[\n]+","",word)]
             else:
-                issueQuery(worker,g)
+                if SINGLE_THREADED:
+                    issueQuery(worker,g)
+                else:
+                    th = threading.Thread(target=populate_todo_archive, args=(worker,g))
+                    th.start()
+                    alltasks += [th]
                 worker = []
         except Exception as e:
             print(e)
@@ -80,5 +93,8 @@ with open("pronounciation.corpus.generated." + str(time.time_ns()) + ".log","a")
     if len(worker):
         issueQuery(worker,g)
         worker = []
-    for word in dict.keys(archive):
-        g.writelines(word) # preserve newlines
+    if len(alltasks):
+        for th in alltasks:
+            th.join()
+    for word in dict.keys(completed_archive):
+        g.writelines(word.lower()) # preserve newlines
