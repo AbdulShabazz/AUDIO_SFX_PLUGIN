cb71bb52ed4efdc90a2bb0496e110901fb07aa09
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-21 01:19:56 -0500

    Update IPATagger.chatgpt.openai.version.3.py

--- Tools/IPATagger.chatgpt.openai.version.3.py
+++ Tools/IPATagger.chatgpt.openai.version.3.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 import time as time
 import openai
 import threading
@@ -7,10 +8,9 @@ import threading
 archive = {}
 todo_archive = {}
 
-RESUME_KEYWORD = ""
-resume_ready = True
-
-def populate_todo_archive():
+def populate_todo_archive(_):
+    RESUME_KEYWORD = ""
+    resume_ready = True
     with open("database.corpus.generated.optimized.log","r") as f:
         for wd in f.readlines():
             if not resume_ready and re.search("^"+RESUME_KEYWORD, wd):
@@ -19,7 +19,7 @@ def populate_todo_archive():
                 wd = re.sub("[\'\.]+", "", wd)
                 todo_archive[wd] = True
 
-def populate_archive():
+def populate_archive(_):
     with open("pronounciation.corpus.generated.1674275980.4888992.log","r") as g:
         for wd in g.readlines():
             wd = re.sub("[_]+", "", wd)
@@ -34,10 +34,17 @@ th_0001.start()
 th_0000.join()
 th_0001.join()
 
+todos = []
+
+with open("pronounciation.corpus.todo.generated.log","w") as f:
+    for word in dict.keys(todo_archive):
+        if word not in archive:
+            todos += [word]
+            f.writelines(word) # preserve newlines
+
 myprompt = '''(1) Seperate each word by a newline, and (2) split each word into syllables, separated by underscores: '''
 
-# openai params
-#
+# OPENAI
 # @param - prompt [prompt.length + response.length < max_tokens]
 # @param - engine [code/text-davinci-ver, code/text-curie-ver, code/text-babbage-ver, code/text-ada-ver]
 # @param - max_tokens [prompt + response < 4097] Length. Bandwidth alloted for the session/connection 
@@ -45,7 +52,7 @@ myprompt = '''(1) Seperate each word by a newline, and (2) split each word into 
 # @param - top_p [1.0 - 0.0] Predictability. Controls the constrained randomness of oncomming text
 # @param - frequency_penalty [1.0 - 0.0] Readability. Controls the ratio of used uncommon words
 # @param - presence_penalty [1.0 - 0.0] DisAllowed repetitiveness/Use synonyms. Ratio of input tokens allowed in the response
-# @returns  { choices[{ engine:[davinci, curie], finish_reason:[stop,length], index:, logprob:, text:[response]},] }
+# @returns  - { choices[{ engine:[davinci, curie], finish_reason:[stop,length], index:N, logprob:[], text:[response]},] }
 def issueQuery(word,g):
     time.sleep(1.1)
     openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -55,23 +62,21 @@ def issueQuery(word,g):
         g.writelines(choice.text + "\n")
         #print(choice.text)
 
-MAX_TOKENS = 4097 # allotted total syllables (prompt + completions)
+MAX_TOKENS = 4097 # allotted total syllables = prompt + completions
 MAX_SEND_TOKENS = 425
 
 with open("pronounciation.corpus.generated." + str(time.time_ns()) + ".log","a") as g:
     worker = []
-    for wd in dict.keys(todo_archive):
-        if wd not in archive:
-            try:
-                if len(worker) < MAX_SEND_TOKENS:
-                    worker += [word]
-                else:
-                    issueQuery(worker,g)
-                    worker = []
-            except Exception as e:
-                print(e)
-                if re.search("exceeds",e):
-                    time.sleep(2)
+    for word in todos:
+        try:
+            if len(worker) < MAX_SEND_TOKENS:
+                worker += [word]
+            else:
+                issueQuery(worker,g)
+                worker = []
+        except Exception as e:
+            print(e)
+            time.sleep(1)
     if len(worker):
         issueQuery(worker,g)
         worker = []
