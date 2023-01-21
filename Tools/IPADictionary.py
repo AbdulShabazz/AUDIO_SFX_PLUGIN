7238de606e0a6782e5add3151151d1bc78540841
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-20 21:51:20 -0500

    Update IPADictionary.py

--- Tools/IPADictionary.py
+++ Tools/IPADictionary.py
@@ -1,92 +1,91 @@
-IPADictionary = {
-    #ordinal
-    "1st": True,
-    "2nd": True,
-    "3rd": True,
-    #"th": True,
-    #Number
-    "0": True,
-    "1": True,
-    "2": True,
-    "3": True,
-    "4": True,
-    "5": True,
-    "6": True,
-    "7": True,
-    "8": True,
-    "9": True,
-    #Letter
-    "a2": True,
-    "b2": True,
-    "c2": True,
-    "d2": True,
-    "e2": True,
-    "f2": True,
-    "g2": True,
-    "h2": True,
-    "i2": True,
-    "j2": True,
-    "k2": True,
-    "l2": True,
-    "m2": True,
-    "n2": True,
-    "o2": True,
-    "p2": True,
-    "q2": True,
-    "r2": True,
-    "s2": True,
-    "t2": True,
-    "u2": True,
-    "v2": True,
-    "w2": True,
-    "x2": True,
-    "y2": True,
-    "z2": True,
-    #Consonant
-    "b": True, # as in "bat"
-    "d": True, # as in "dog"
-    "f": True, # as in "fish"
-    "g": True, # as in "go"
-    "h": True, # as in "hat"
-    "j": True, # as in "jump, budget"
-    "k": True, # as in "cat"
-    "l": True, # as in "lip"
-    "m": True, # as in "mat"
-    "n": True, # as in "nap"
-    "ng": True, # as in "sing"
-    "p": True, # as in "pat"
-    "r": True, # as in "rip"
-    "s": True, # as in "sip"
-    "sh": True, # as in "sheep"
-    "t": True, # as in "tap"
-    "ch": True, # as in "church, witch"
-    "th": True, # as in "think, math, nothing"
-    "th2": True, # as in "this, breathe"
-    "v": True, # as in "vat"
-    "w": True, # as in "wet"
-    "hw": True, # as in "where, somewhat"
-    "y": True, # as in "yacht, yes, onion"
-    "z": True, # as in "zoo, zip, easy"
-    "z2": True, # as in "measure, television, beige"
+IPA_Dictionary = ( 
+
+    #Consonant = Sounds like
+    "b", # as in "bat"
+    "d", # as in "dog"
+    "f", # as in "fish"
+    "g", # as in "go"
+    "h", # as in "hat"
+    "j", # as in "jump, budget"
+    "k", # as in "cat"
+    "l", # as in "lip"
+    "m", # as in "mat"
+    "n", # as in "nap"
+    "p", # as in "pat"
+    "r", # as in "rip"
+    "s", # as in "sip"
+    "sh", # as in "sheep"
+    "t", # as in "tap"
+    "ch", # as in "church, witch"
+    "th", # as in "think, math, nothing"
+    "th2", # as in "this, breathe"
+    "v", # as in "vat"
+    "w", # as in "wet"
+    "hw", # as in "where, somewhat"
+    "y", # as in "yacht, yes, onion"
+    "z", # as in "zoo, zip, easy"
+    "z2", # as in "measure, television, beige"
+
     #"Vowel":
-    "a": True, # as in "apple, hat, bat"
-    "ey": True, # as in "aid, bait, day"
-    "ah": True, # as in "arm, father, aha"
-    "air": True, # as in "air, careful, wear"
-    "aw": True, # as in "bought"
-    "e": True, # as in "bet, ever, head"
-    "ee": True, # as in "beet, eat, seed, need"
-    "eer": True, # as in "teacher, murderer"
-    "i": True, # as in "it, finishes"
-    "ahy": True, # as in "I, ice, deny"
-    "o": True, # as in "odd, waffle"
-    "oh": True, # as in "owe, road"
-    "oo": True, # as in "rude, soup"
-    "oo2": True, # as in "good, book"
-    "ou": True, # as in "out, how, loud"
-    "uh": True, # as in "up, mother"
-    "uh2": True, # as in "about, problem, circus"
-    "ur": True # as in "early, stirring"
-}
+    "a", # as in "apple, hat, bat"
+    "a2", # as in "aid, bait, day"
+    "a3", # as in "arm, father, aha"
+    "e", # as in "bet, ever, head"
+    "e2", # as in "beet, eat, seed, need"
+    "i", # as in "it, finishes"
+    "i2", # as in "I, ice, deny"  
+    "i3", # as in first, thirst
+    "o", # as in "odd, waffle"
+    "o2", # as in "letter 'o', owe, road"
+    "o3", # as in "good, book"
+    "u", # as in "up, mother"
+    "u2", # as in rude, brute  
+    
+    #Letter = Sounds like
+    "a2", # Sounds like "A",
+    "b2", # Sounds like "B",
+    "c2", # Sounds like "C",
+    "d2", # Sounds like "D",
+    "e2", # Sounds like "E",
+    "f2", # Sounds like "F",
+    "g2", # Sounds like "G",
+    "h2", # Sounds like "H",
+    "i2", # Sounds like "I",
+    "j2", # Sounds like "J",
+    "k2", # Sounds like "K",
+    "l2", # Sounds like "L",
+    "m2", # Sounds like "M",
+    "n2", # Sounds like "N",
+    "o2", # Sounds like "O",
+    "p2", # Sounds like "P",
+    "q2", # Sounds like "Q",
+    "r2", # Sounds like "R",
+    "s2", # Sounds like "S",
+    "t2", # Sounds like "T",
+    "u2", # Sounds like "U",
+    "v2", # Sounds like "V",
+    "w2", # Sounds like "W",
+    "x2", # Sounds like "X",
+    "y2", # Sounds like "Y",
+    "z2", # Sounds like "Z",
+    
+    #ordinal
+    "1st", # Sounds like "f_i3_r_s_t",
+    "2nd", # Sounds like # "s_e_k_o_n_d",
+    "3rd", # Sounds like # "th_i3_r_d",
+    "D*Nth", # Where D contains zero or more digits, and N is a digit greater than 4, as 104th, 4th, 1004th, 106th,
+
+    #Number
+    "0", # Sounds like "z2_e2_r_o2",
+    "1", # Sounds like "w_o_n",
+    "2", # Sounds like "t_o3",
+    "3", # Sounds like "th_r_e2",
+    "4", # Sounds like "f_o2_r",
+    "5", # Sounds like "f_i2_v",
+    "6", # Sounds like "s_i_k_s",
+    "7", # Sounds like "s_e_v_n2",
+    "8", # Sounds like "a2_t",
+    "9", # Sounds like "n_i2_n", 
+)
 
 # Ex 'IPADictionary["ch"] # (True)', 'IPADictionary["a"] # (True)'
