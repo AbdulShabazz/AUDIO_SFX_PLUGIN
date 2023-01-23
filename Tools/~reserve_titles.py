3ebd032dd1bc0f45591a711efe59e186af3e76ec
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-23 02:49:03 -0500

    Update ~reserve_titles.py

--- Tools/~reserve_titles.py
+++ Tools/~reserve_titles.py
@@ -1,3 +1,22 @@
+import re
+import json
+
+result = {}
+corpus = {}
+
+with open("pronunciation.corpus.generated.1.log","r") as f:
+    result = json.loads( f.read())
+
+for wd in dict.keys(result):
+    corpus[ re.sub("[_]+","",wd)] = "_".join( list( wd))
+
+with open("pronunciation.corpus.generated.4.log","w") as g:
+    g.writelines( json.dumps( corpus, indent=2))
+
+print("Q.E.D.")
+
+#
+
 result = []
 
 with open("pronunciation.corpus.generated.2.log","r") as f:
