1435256c183cb98189e052f6ab75982dc10fa437
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-01-31 01:31:52 -0500

    Create AUDIO_SFX_PLUGIN.HEADERS.h

---
+++ Source/AUDIO_SFX_PLUGIN/Public/AUDIO_SFX_PLUGIN.HEADERS.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// AUDIO_SFX_PLUGIN includes
+
+#include <memory>
+#include <GameFramework/PlayerController.h>
+#include <Camera/CameraActor.h>
+#include "AUDIO_SFX_PLUGINCommands.h"
+#include "CoreMinimal.h"
+#include "Modules/ModuleManager.h"
+#include <GameFramework/Actor.h>
+#include <Engine/Classes/Engine/Engine.h>
+#include <Engine/World.h>
+#include "AUDIO_SFX_PLUGINStyle.h"
+#include "LevelEditor.h"
+#include "Widgets/Docking/SDockTab.h"
+#include "Widgets/Layout/SBox.h"
+#include "Widgets/Text/STextBlock.h"
+#include "ToolMenus.h"
+#include <stdexcept>
+#include <EditorStyle.h>
