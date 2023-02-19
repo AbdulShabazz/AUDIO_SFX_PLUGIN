881f7e3ecc3c90b2302608322a099dfd97f2c6f6
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:40:47 -0500

    Create GameAnalytics.h

---
+++ Tools/NewUE5Classes/GameAnalytics.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FGameAnalyticsClass
+{
+/**  A view or tool that would provide real-time analytics about game performance, player behavior, and other metrics could help developers make more informed decisions about their projects.
+ */
+public:
+	FGameAnalyticsClass ()
+	{
+		
+	};
+};
