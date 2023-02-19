ee36436b8424cf28bd71c76d9703e73e1f8b556c
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:41:19 -0500

    Create TimelineEditing.h

---
+++ Tools/NewUE5Classes/TimelineEditing.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FTimelineEditingClass
+{
+/**  A view or tool that would allow developers to create and edit visual timelines for game events and animations would help simplify the process of designing and implementing game events.
+ */
+public:
+	FTimelineEditingClass ()
+	{
+		
+	};
+};
