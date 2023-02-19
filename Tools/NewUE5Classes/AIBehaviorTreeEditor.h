2cf351bbdd4be5e15b63b7191428f81c80ae0ee3
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:40:18 -0500

    Create AIBehaviorTreeEditor.h

---
+++ Tools/NewUE5Classes/AIBehaviorTreeEditor.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FAIBehaviorTreeEditorClass
+{
+/**  While Unreal Engine already has AI visual scripting tools, a dedicated behavior tree editor would make it easier for developers to create complex AI behaviors for their games.
+ */
+public:
+	FAIBehaviorTreeEditorClass ()
+	{
+		
+	};
+};
