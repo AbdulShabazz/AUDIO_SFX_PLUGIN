87692c863b4bb676cbe9147aab204e687443cb01
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:40:39 -0500

    Create CustomizableInterface.h

---
+++ Tools/NewUE5Classes/CustomizableInterface.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FCustomizableInterfaceClass
+{
+/**  While Unreal Engine's interface is already highly customizable, more options for customizing the look and feel of the interface could be added. This could include the ability to create custom views or rearrange the interface to better suit individual workflows.
+ */
+public:
+	FCustomizableInterfaceClass ()
+	{
+		
+	};
+};
