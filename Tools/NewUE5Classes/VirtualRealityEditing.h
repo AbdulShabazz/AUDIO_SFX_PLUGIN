17a4e2c5fe59eeac3af222132515c56143563ea3
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:41:14 -0500

    Create VirtualRealityEditing.h

---
+++ Tools/NewUE5Classes/VirtualRealityEditing.h
@@ -0,0 +1,11 @@
+#pragma once;
+
+class FVirtualRealityEditingClass
+{
+/**  Adding a view or tool that would allow developers to create and edit virtual reality experiences within the engine could help make it easier for developers to create immersive VR games and applications. */
+public:
+	FVirtualRealityEditingClass ()
+	{
+		
+	};
+};
