c412bb646e780b4e178154fe345470751710438b
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:40:43 -0500

    Create DebuggingandProfiling.h

---
+++ Tools/NewUE5Classes/DebuggingandProfiling.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FDebuggingandProfilingClass
+{
+/**  A more comprehensive set of tools to help developers debug and profile their game code would be beneficial. This could include a dedicated debugging view or a profiling tool to help identify performance bottlenecks and optimize the game.
+ */
+public:
+	FDebuggingandProfilingClass ()
+	{
+		
+	};
+};
