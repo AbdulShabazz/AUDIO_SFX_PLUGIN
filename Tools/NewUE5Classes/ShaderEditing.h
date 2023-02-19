3dd049a11442e7cd16b76692722a03826c6c460b
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:41:23 -0500

    Create ShaderEditing.h

---
+++ Tools/NewUE5Classes/ShaderEditing.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FShaderEditingClass
+{
+/**  A view or tool that would allow developers to create and edit custom shaders within the engine could help create more complex and sophisticated visual effects.
+ */
+public:
+	FShaderEditingClass ()
+	{
+		
+	};
+};
