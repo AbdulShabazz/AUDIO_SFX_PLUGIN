04bb60fa445fb0ac6b770f3b6d53c483b9026469
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:40:53 -0500

    Create LocalizationManagement.h

---
+++ Tools/NewUE5Classes/LocalizationManagement.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FLocalizationManagementClass
+{
+/**  A view or tool that would help developers manage and test translations of game content across multiple languages would be useful for developers creating games for global audiences.
+ */
+public:
+	FLocalizationManagementClass ()
+	{
+		
+	};
+};
