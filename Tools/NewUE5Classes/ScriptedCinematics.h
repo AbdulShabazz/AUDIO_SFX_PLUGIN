5661311105ba1b0ac065ea877727e7f1d93171cf
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:41:06 -0500

    Create ScriptedCinematics.h

---
+++ Tools/NewUE5Classes/ScriptedCinematics.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FScriptedCinematicsClass
+{
+/**  Adding a view or tool to allow developers to create and edit scripted cinematics within the engine could help simplify the process of creating cinematic cutscenes for games.
+ */
+public:
+	FScriptedCinematicsClass ()
+	{
+		
+	};
+};
