9b169c5c0b3ab2d479299b4e82f394407a7a05ae
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:40:30 -0500

    Create AudioEditing.h

---
+++ Tools/NewUE5Classes/AudioEditing.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FAudioEditingClass
+{
+/**  A view or tool that would allow developers to edit and mix game audio, including sound effects and music, within the engine would be a valuable addition.
+ */
+public:
+	FAudioEditingClass ()
+	{
+		
+	};
+};
