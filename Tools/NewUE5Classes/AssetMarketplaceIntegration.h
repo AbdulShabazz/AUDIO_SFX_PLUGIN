8deae7765175c14d528d3dbbb64929d481620b1b
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:40:27 -0500

    Create AssetMarketplaceIntegration.h

---
+++ Tools/NewUE5Classes/AssetMarketplaceIntegration.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FAssetMarketplaceIntegrationClass
+{
+/**  Adding an integrated marketplace to the Unreal Engine interface would make it easier for developers to discover, purchase, and import assets into their projects.
+ */
+public:
+	FAssetMarketplaceIntegrationClass ()
+	{
+		
+	};
+};
