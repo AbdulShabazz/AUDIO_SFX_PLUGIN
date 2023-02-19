ab3ee510c9dbd57ee8bb1fbcb269c8616742c263
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:40:59 -0500

    Create ProceduralGeneration.h

---
+++ Tools/NewUE5Classes/ProceduralGeneration.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FProceduralGenerationClass
+{
+/**  A view or tool that would allow developers to create procedural content, such as terrain, buildings, or landscapes, would be a great addition to the engine.
+ */
+public:
+	FProceduralGenerationClass ()
+	{
+		
+	};
+};
