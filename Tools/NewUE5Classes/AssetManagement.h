f155564cd739584c2f0149fa66792f33762e9688
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:40:21 -0500

    Create AssetManagement.h

---
+++ Tools/NewUE5Classes/AssetManagement.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FAssetManagementClass
+{
+/**  While Unreal Engine already has a Content Browser, it could be expanded to provide more features for managing and organizing game assets. For example, a version control system or a tool to help locate and resolve asset dependencies could be useful.
+ */
+public:
+	FAssetManagementClass ()
+	{
+		
+	};
+};
