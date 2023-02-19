6a85a400514cf887e06c75e767ee3ff54fc854a2
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-19 18:41:10 -0500

    Create VisualScriptingforGameMechanics.h

---
+++ Tools/NewUE5Classes/VisualScriptingforGameMechanics.h
@@ -0,0 +1,12 @@
+#pragma once;
+
+class FVisualScriptingforGameMechanicsClass
+{
+/**  While Unreal Engine already has a robust visual scripting system through its Blueprint Editor, adding more tools to create complex game mechanics would be a great addition. For example, a visual programming tool specifically focused on AI or physics would be useful.
+ */
+public:
+	FVisualScriptingforGameMechanicsClass ()
+	{
+		
+	};
+};
