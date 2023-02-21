26a8c1c7c2d464223f2a0240e5a713edaaa4c12b
AbdulShabazz <39221089+AbdulShabazz@users.noreply.github.com>
2023-02-20 19:02:35 -0500

    Update InlineClassTool.py

--- Tools/InlineClassTool.py
+++ Tools/InlineClassTool.py
@@ -1,9 +1,18 @@
 import re
 
 description = '''
-Here's a possible implementation of the Python module that reads 
-the C++20 header class file and its implementation file and returns 
+
+Here is one implementation of a Python module that reads 
+the C++20 header class file, and its implementation file, and returns 
 a new header file with all class member methods and attributes inlined:
+
+Example usage:
+
+	h_file = f'[class_name].h'
+	cpp_file = f'[class_name].cpp'
+
+	out_h = inline_class(h_file, cpp_file)
+
 '''
 
 def inline_class(header_file, impl_file):
@@ -14,7 +23,7 @@ def inline_class(header_file, impl_file):
     with open(impl_file, 'r') as f:
         impl_text = f.read()
     # extract class name
-    class_name_match = re.search(r'class\s+(\w+)\s*{', header_text)
+    class_name_match = re.search(r'class\s+(\w+)\s*\n*{', header_text)
     if not class_name_match:
         raise ValueError('Class name not found in header file')
     class_name = class_name_match.group(1)
@@ -33,7 +42,7 @@ def inline_class(header_file, impl_file):
     for attribute_match in attribute_matches:
         attributes.append((attribute_match.group(1), attribute_match.group(2)))
     # extract method implementation
-    method_impl_regex = r'^\s*void\s+' + class_name + '::(\w+)\((.*)\)\s*\{\s*(.*?)\s*\}'
+    method_impl_regex = r'^\s*void\s+' + class_name + '::(\w+)\((.*)\)\s*\n*\{\s*(.*?)\s*\}'
     method_impl_matches = re.finditer(method_impl_regex, impl_text, re.DOTALL)
     method_impls = {}
     for method_impl_match in method_impl_matches:
